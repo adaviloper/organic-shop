f10ed6943596a771c669e94e08d91ed08e54ecb6
adrian <16654653+adaviloper@users.noreply.github.com>
2021-04-02 18:59:34 -0500

    281 - Defining the Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,71 @@ import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AppNavComponent } from './app-nav/app-nav.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
     BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'products',
+        component: ProductsComponent,
+      },
+      {
+        path: 'shopping-cart',
+        component: ShoppingCartComponent,
+      },
+      {
+        path: 'check-out',
+        component: CheckOutComponent,
+      },
+      {
+        path: 'order-success',
+        component: OrderSuccessComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'admin/products',
+        component: AdminProductsComponent,
+      },
+      {
+        path: 'admin/orders',
+        component: AdminOrdersComponent,
+      },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
